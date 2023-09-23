4e4aee40fff72d9b14064e485ee29d6b09141027
Mela-nina <oge4sym@gmail.com>
2023-09-23 09:06:13 +0000

    Task 7

--- models/user.py
+++ models/user.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """This module will define a class User"""
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
     """This class will define a user by various attributes"""
     __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', backref='user', cascade='all, delete, delete-orphan')
+    reviews = relationship('Review', backref='user', cascade='all, delete, delete-orphan')
