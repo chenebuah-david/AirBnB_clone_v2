470753896a18f6625ede8eb94b694903b2d73056
Mela-nina <oge4sym@gmail.com>
2023-10-19 08:46:45 +0000

    web_flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Start the Flask app with root route
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    This returns a string saying Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This returns a string saying HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_with_text(text):
+    """
+    This displays "C", followed by value of <text>
+    """
+    tokens = text.split("_")
+    text = "C " + " ".join(tokens)
+    return text
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
